ba06d11aa7986ee14d39b85bac6ebf535df1fc3d
René Köcher <shirk@bitspin.org>
2011-09-28 14:21:01 +0200

    Tweak default.html to include an ajaxy porject list.

---
+++ js/github-api.js
@@ -0,0 +1,23 @@
+/* jQuery powered code to pull in a github repository list */
+
+function parseRepos(data) {
+	var projects = [];
+	data.sort(function(a, b) { return a.name > b.name; });
+
+	for (repo in data) {
+		if (data[repo].name === 'shirk.github.com') {
+			continue;
+		}
+		projects.push('<li><a href="' + data[repo].html_url + '">::' + data[repo].name + '</a></li>');
+	}
+
+	container = document.getElementById('ghProjectList');
+	if (!(container === undefined)) {
+		$('<ul/>', { html : projects.join('') }).appendTo(container);
+	}
+}
+
+$(document).ready(function(){
+	$.getJSON('https://api.github.com/users/Shirk/repos', parseRepos)
+})
+
