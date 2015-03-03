a8f5a1e5d4d78b6b169128a7c071c376ed7368dd
René Köcher <shirk@bitspin.org>
2015-03-03 08:38:01 +0100

    Filter projects by original and contributions.

--- js/github-api.js
+++ js/github-api.js
@@ -2,6 +2,7 @@
 
 function parseRepos(data) {
 	var projects = [];
+	var contribs = [];
 
 	data = data.data;
 	data.sort(function(a, b) { return a.name > b.name; });
@@ -10,12 +11,20 @@ function parseRepos(data) {
 		if (data[repo].name === 'shirk.github.com') {
 			continue;
 		}
-		projects.push('<li><a href="' + data[repo].html_url + '">::' + data[repo].name + '</a></li>');
+		if (data[repo].fork == true ) {
+			contribs.push('<li><a href="' + data[repo].html_url + '">::' + data[repo].name + '</a></li>');
+		} else {
+			projects.push('<li><a href="' + data[repo].html_url + '">::' + data[repo].name + '</a></li>');
+		}
 	}
 
 	container = document.getElementById('ghProjectList');
 	if (!(container === undefined)) {
 		$('<ul/>', { html : projects.join('') }).appendTo(container);
+	}
+	container = document.getElementById('ghContribList');
+	if (!(container === undefined)) {
+		$('<ul/>', { html : contribs.join('') }).appendTo(container);
 	}
 }
 
